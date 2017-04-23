4b08a8cdc8eb0adde7ca83258d0c1ca907608134
DukeCode <jid17@pitt.edu>
2017-04-23 02:09:34 -0400

    t

--- src/main/java/edu/infsci2560/MvcConfig.java
+++ src/main/java/edu/infsci2560/MvcConfig.java
@@ -10,7 +10,7 @@ public class MvcConfig extends WebMvcConfigurerAdapter {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/home").setViewName("home");
-        registry.addViewController("/").setViewName("land");
+        registry.addViewController("/").setViewName("home");
         registry.addViewController("/allAnalysis").setViewName("allAnalysis");
         registry.addViewController("/allBookComments").setViewName("allBookComments");
         registry.addViewController("/allSharedBooks").setViewName("allSharedBooks");
